e71ab2fe606eea05f405296ed10182086b6c6f67
Andrey Belov <belovandr96@gmail.com>
2023-04-21 14:28:41 +0500

    Third commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("It's a second commit!");
+        System.out.println("It's a third commit!");
     }
 }
